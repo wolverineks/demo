552f4a3b27d46728827e70b12bdaff451c527b48
Kevin Sullivan <wolverine.ks@gmail.com>
2020-07-22 08:36:20 -0700

    update

--- src/utils/getCurrencySymbol.ts
+++ src/utils/getCurrencySymbol.ts
@@ -1,6 +1,6 @@
 import { EdgeAccount } from 'edge-core-js'
 
-import { getCurrencyInfoFromWalletType } from './getCurrencyInfoFromWalletType'
+import { getCurrencyInfoFromWalletType } from '../utils'
 
 export const getCurrencySymbol = (account: EdgeAccount, { walletType }: { walletType: string }) =>
   getCurrencyInfoFromWalletType({ account, walletType }).denominations[0]?.symbol
