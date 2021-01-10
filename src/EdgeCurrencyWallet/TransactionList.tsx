78ba277f67000d0d5ae062ec55ed9c3c6b1ac135
Kevin Sullivan <wolverine.ks@gmail.com>
2021-01-10 12:04:57 -0800

    Fix filter, clean up

--- src/EdgeCurrencyWallet/TransactionList.tsx
+++ src/EdgeCurrencyWallet/TransactionList.tsx
@@ -6,13 +6,14 @@ import { useEdgeAccount } from '../auth'
 import { Boundary, DisplayAmount } from '../components'
 import { useDisplayDenomination, useTransactionCount, useTransactions } from '../hooks'
 import { exchangeToNative, getTxUrl, nativeToDenominated } from '../utils'
-
-const normalize = (text: string) => text.trim().toLowerCase()
+import { useFilter } from './useFilter'
 
 const matches = (query: string) => (transaction: EdgeTransaction): boolean => {
+  const normalize = (text: string) => text.trim().toLowerCase()
+
   return (
     transaction.txid.includes(query) ||
-    new Date(transaction.date).toLocaleString().includes(query) ||
+    normalize(new Date(transaction.date * 1000).toLocaleString()).includes(normalize(query)) ||
     normalize(transaction.currencyCode).includes(normalize(query)) ||
     normalize(transaction.nativeAmount).includes(normalize(query)) ||
     normalize(transaction.metadata?.name || '').includes(normalize(query)) ||
@@ -26,24 +27,23 @@ export const TransactionList: React.FC<{ wallet: EdgeCurrencyWallet; currencyCod
   wallet,
   currencyCode,
 }) => {
-  const [searchQuery, setSearchQuery] = React.useState('')
   const transactionCount = useTransactionCount(wallet, { currencyCode })
-  const transactions = useTransactions(wallet, { currencyCode })
-  const visibleTransactions = transactions.filter(matches(searchQuery))
+  const [transactions, setFilterQuery] = useFilter(matches, useTransactions(wallet, { currencyCode }))
+  const dedupe = (transactions: EdgeTransaction[]) =>
+    Object.values<EdgeTransaction>(
+      transactions.reduce((result, transaction) => ({ ...result, [transaction.txid]: transaction }), {}),
+    )
 
   return (
     <ListGroup>
       <ListGroup>
-        Transactions: {transactionCount} ({visibleTransactions.length})
-        <FormControl placeholder={'Search'} onChange={(event) => setSearchQuery(event.currentTarget.value)} />
-        {transactionCount <= 0 ? (
+        Transactions: {transactionCount} ({dedupe(transactions).length})
+        <FormControl placeholder={'Search'} onChange={(event) => setFilterQuery(event.currentTarget.value)} />
+        {dedupe(transactions).length <= 0 ? (
           <div>No Transactions</div>
         ) : (
-          visibleTransactions.map((transaction) => (
-            <TransactionListRow
-              transaction={transaction as EdgeTransaction}
-              key={(transaction as EdgeTransaction).txid}
-            />
+          dedupe(transactions).map((transaction) => (
+            <TransactionListRow transaction={transaction} key={transaction.txid} />
           ))
         )}
       </ListGroup>
@@ -62,20 +62,7 @@ const TransactionListRow: React.FC<{ transaction: EdgeTransaction }> = ({ transa
         {transaction.txid}
       </span>
 
-      {transaction.metadata && (
-        <div>
-          <div>Name: {transaction.metadata.name}</div>
-          <div>Category: {transaction.metadata.category}</div>
-          <div>Notes: {transaction.metadata.notes}</div>
-          <div>Exchange Amounts:</div>
-          {transaction.metadata.exchangeAmount &&
-            Object.entries(transaction.metadata.exchangeAmount).map(([currencyCode, exchangeAmount]) => (
-              <Boundary key={currencyCode}>
-                <DisplayFiatAmount currencyCode={currencyCode} exchangeAmount={exchangeAmount} />
-              </Boundary>
-            ))}
-        </div>
-      )}
+      {transaction.metadata && <Metadata metadata={transaction.metadata} />}
 
       <NavLink target={'none'} href={getTxUrl(account, transaction)}>
         View Details
@@ -84,7 +71,35 @@ const TransactionListRow: React.FC<{ transaction: EdgeTransaction }> = ({ transa
   )
 }
 
-const DisplayFiatAmount: React.FC<{ currencyCode: string; exchangeAmount: number | string }> = ({
+const Metadata = ({ metadata }: { metadata: NonNullable<EdgeTransaction['metadata']> }) => {
+  return (
+    <div>
+      <div>Name: {metadata.name}</div>
+      <div>Category: {metadata.category}</div>
+      <div>Notes: {metadata.notes}</div>
+      <div>Exchange Amounts:</div>
+      {metadata.exchangeAmount && <ExchangeAmounts exchangeAmounts={metadata.exchangeAmount} />}
+    </div>
+  )
+}
+
+const ExchangeAmounts = ({
+  exchangeAmounts,
+}: {
+  exchangeAmounts: NonNullable<NonNullable<EdgeTransaction['metadata']>['exchangeAmount']>
+}) => {
+  return (
+    <>
+      {Object.entries(exchangeAmounts).map(([currencyCode, exchangeAmount]) => (
+        <Boundary key={currencyCode}>
+          <ExchangeAmount currencyCode={currencyCode} exchangeAmount={exchangeAmount} />
+        </Boundary>
+      ))}
+    </>
+  )
+}
+
+const ExchangeAmount: React.FC<{ currencyCode: string; exchangeAmount: number | string }> = ({
   currencyCode,
   exchangeAmount,
 }) => {
