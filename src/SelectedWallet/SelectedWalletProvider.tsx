c2b410a6b7291bec55edecc9eb0695efdbe256d2
Kevin Sullivan <wolverine.ks@gmail.com>
2021-01-28 09:41:46 -0600

    clean up SelectedWalletProvider

--- src/SelectedWallet/SelectedWalletProvider.tsx
+++ src/SelectedWallet/SelectedWalletProvider.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import { useQueryClient } from 'react-query'
 
 import { useEdgeAccount } from '../auth'
-import { useEdgeCurrencyWallet } from '../hooks'
+import { useActiveWalletIds, useEdgeCurrencyWallet, useTokens } from '../hooks'
 import { getCurrencyCodeFromWalletId } from '../utils'
 
 type SelectedWalletInfo = { id: string; currencyCode: string }
-type SetSelectedWalletInfo = (selectedWalletInfo: SelectedWalletInfo) => void
+type SetSelectedWalletInfo = (selectedWalletInfo?: SelectedWalletInfo) => void
 
 const SelectedWalletInfoContext = React.createContext<
   Readonly<[SelectedWalletInfo | undefined, SetSelectedWalletInfo]> | undefined
@@ -14,46 +13,14 @@ const SelectedWalletInfoContext = React.createContext<
 
 export const SelectedWalletInfoProvider: React.FC = ({ children }) => {
   const account = useEdgeAccount()
-  const [selectedWalletId, setSelectedWalletId] = React.useState<string | undefined>(account.activeWalletIds[0])
-  const [selectedCurrencyCode, setSelectedCurrencyCode] = React.useState<string | undefined>(
-    selectedWalletId ? getCurrencyCodeFromWalletId(account, selectedWalletId) : undefined,
-  )
-
-  const setSelectedWalletInfo: SetSelectedWalletInfo = React.useCallback(({ id, currencyCode }) => {
-    setSelectedWalletId(id)
-    setSelectedCurrencyCode(currencyCode)
-  }, [])
-
-  // NO SELECTED WALLET / NO ACTIVE WALLET IDS
-  let selectedWalletInfo: SelectedWalletInfo | undefined
-
-  // SELECTED, ACTIVE
-  if (selectedWalletId && selectedCurrencyCode && account.activeWalletIds.includes(selectedWalletId))
-    selectedWalletInfo = { id: selectedWalletId, currencyCode: selectedCurrencyCode }
-
-  const queryClient = useQueryClient()
-  // if token is disabled while selected, clear
-  React.useEffect(() => {
-    if (!selectedWalletId || !selectedCurrencyCode) return
-
-    const unsubscribe = queryClient.getQueryCache().subscribe(async (query) => {
-      if (!query) return
-      const match = query.queryKey[0] === selectedWalletId && query.queryKey[1] === 'enabledTokens'
-
-      if (match) {
-        const wallet = await account.waitForCurrencyWallet(selectedWalletId)
-        const currencyInfo = wallet.currencyInfo
-        const tokens = await wallet.getEnabledTokens()
-
-        if (![...tokens, currencyInfo.currencyCode].includes(selectedCurrencyCode)) {
-          setSelectedWalletId(undefined)
-          setSelectedCurrencyCode(undefined)
+  const [selectedWalletInfo, setSelectedWalletInfo] = React.useState<SelectedWalletInfo | undefined>(
+    account.activeWalletIds[0]
+      ? {
+          id: account.activeWalletIds[0],
+          currencyCode: getCurrencyCodeFromWalletId(account, account.activeWalletIds[0]),
         }
-      }
-    })
-
-    return unsubscribe
-  }, [account, queryClient, selectedCurrencyCode, selectedWalletId])
+      : undefined,
+  )
 
   return (
     <SelectedWalletInfoContext.Provider value={[selectedWalletInfo, setSelectedWalletInfo] as const}>
@@ -73,7 +40,20 @@ export const useSelectedWallet = () => {
 
   if (!walletInfo) throw new Error('Missing SelectedWallet Boundary')
 
-  const wallet = useEdgeCurrencyWallet({ account: useEdgeAccount(), walletId: walletInfo.id })
+  const account = useEdgeAccount()
+  const wallet = useEdgeCurrencyWallet({ account, walletId: walletInfo.id })
+
+  // unselect wallet if edgeWallet is deactivated
+  const activeWalletIds = useActiveWalletIds(account)
+  if (!activeWalletIds.includes(walletInfo.id)) {
+    selectWallet()
+  }
+
+  // unselect wallet if selected currency code is deactivated
+  const { enabledTokens } = useTokens(account, wallet)
+  if (![wallet.currencyInfo.currencyCode, ...enabledTokens].includes(walletInfo.currencyCode)) {
+    selectWallet()
+  }
 
   return [{ wallet, id: walletInfo?.id, currencyCode: walletInfo?.currencyCode }, selectWallet] as const
 }
