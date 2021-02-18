0d1ed0422c49b6b73fb38bacb09d45713253b55c
Kevin Sullivan <wolverine.ks@gmail.com>
2021-02-18 14:18:51 -0600

    fix explorer urls

--- src/hooks/transactions.ts
+++ src/hooks/transactions.ts
@@ -1,19 +1,23 @@
 import { EdgeAccount, EdgeTransaction } from 'edge-core-js'
 
+import { Explorers } from './tokens'
 import { useInfo } from './useInfo'
 
 export const useTransactionExplorerUrl = (account: EdgeAccount, transaction: EdgeTransaction) => {
-  return (useInfo(account, transaction.currencyCode) as any).transactionExplorer?.replace('%s', transaction.txid)
+  return (useInfo(account, transaction.currencyCode) as Explorers).transactionExplorer?.replace('%s', transaction.txid)
 }
 
 export const useAddressExplorerUrl = (account: EdgeAccount, transaction: EdgeTransaction) => {
-  return (useInfo(account, transaction.currencyCode) as any).addressExplorer?.replace('%s', transaction.txid)
+  return (useInfo(account, transaction.currencyCode) as Explorers).addressExplorer?.replace(
+    '%s',
+    transaction.ourReceiveAddresses[0],
+  )
 }
 
 export const useBlockExplorerUrl = (account: EdgeAccount, transaction: EdgeTransaction) => {
-  return (useInfo(account, transaction.currencyCode) as any).addressExplorer?.replace('%s', transaction.txid)
+  return (useInfo(account, transaction.currencyCode) as Explorers).blockExplorer?.replace('%s', transaction.txid)
 }
 
 export const useXpubExplorerUrl = (account: EdgeAccount, transaction: EdgeTransaction) => {
-  return (useInfo(account, transaction.currencyCode) as any).addressExplorer?.replace('%s', transaction.txid)
+  return (useInfo(account, transaction.currencyCode) as Explorers).xpubExplorer?.replace('%s', transaction.txid)
 }
